004fead726fc6d33e0ad55b050472486bf8e3fff
nx20 <yudv08@gmail.com>
2025-01-01 16:13:45 +0900

    change index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const colors = ['31', '32', '33', '34'];
 
